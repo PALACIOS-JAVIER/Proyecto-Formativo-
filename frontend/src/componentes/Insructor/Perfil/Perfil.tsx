866ca5fc678ad90bfe1abb65f0afa1bfb43f0432
Arcos2819 <arcosmunozemiliano719@gmail.com>
2026-07-15 15:01:19 -0500

    Actualización Perfil

--- frontend/src/componentes/Insructor/Perfil/Perfil.tsx
+++ frontend/src/componentes/Insructor/Perfil/Perfil.tsx
@@ -1,1 +1,13 @@
-import react from "react";
+
+
+function Perfil() {
+    return (
+        <div>
+            <h1>Perfil del Instructor</h1>
+            <p>Bienvenido al perfil del instructor. Aquí puedes ver y editar tu información personal.</p>
+        </div>
+
+    );
+}
+
+export default Perfil;
