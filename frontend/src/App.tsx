31671c1b44515fcb9a6d577d29afc8ebbc99dc78
chenaoh <3138125060javier@gmail.com>
2026-07-15 14:01:30 -0500

    Se hizo ajustes de carpertas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import './App.css'
-import { Login } from './Login'
+import { Login } from './login/Login'
 
 function App() {
   const handleLogin = ({ username, password }: { username: string; password: string }) => {
