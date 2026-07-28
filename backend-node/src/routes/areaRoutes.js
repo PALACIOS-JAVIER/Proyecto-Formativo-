60b3a36a707175b951abb37a0fe8a2fa9841d173
Yuliana Samboni <samboniandryyuliana@gmail.com>
2026-07-28 15:53:48 -0500

    se elimino carpeta mal creada

--- backend-node/src/routes/areaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAreas } = require('../controllers/areaController');
-
-const router = express.Router();
-
-// Endpoint público para que el frontend pueda cargar las áreas en el registro
-router.get('/', getAreas);
-
-module.exports = router;
