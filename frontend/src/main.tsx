f4b5e5523613f0a61f906109bed42f1fc793e052
chenaoh <3138125060javier@gmail.com>
2026-08-25 00:28:22 -0500

    Fix typescript strict type checking errors for production build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { ErrorBoundary } from './componentes/common/ErrorBoundary'
 // Workaround para evitar que Google Translate rompa (ponga en blanco) la aplicación React
 if (typeof Node === 'function' && Node.prototype) {
   const originalRemoveChild = Node.prototype.removeChild;
-  Node.prototype.removeChild = function (child: Node): Node {
+  Node.prototype.removeChild = function (this: Node, child: Node): Node {
     if (child.parentNode !== this && child.parentNode) {
       return originalRemoveChild.call(child.parentNode, child);
     }
@@ -16,7 +16,7 @@ if (typeof Node === 'function' && Node.prototype) {
   } as typeof Node.prototype.removeChild;
 
   const originalInsertBefore = Node.prototype.insertBefore;
-  Node.prototype.insertBefore = function (newNode: Node, referenceNode: Node | null): Node {
+  Node.prototype.insertBefore = function (this: Node, newNode: Node, referenceNode: Node | null): Node {
     if (referenceNode && referenceNode.parentNode !== this && referenceNode.parentNode) {
       return originalInsertBefore.call(referenceNode.parentNode, newNode, referenceNode);
     }
@@ -24,7 +24,7 @@ if (typeof Node === 'function' && Node.prototype) {
   } as typeof Node.prototype.insertBefore;
 
   const originalReplaceChild = Node.prototype.replaceChild;
-  Node.prototype.replaceChild = function (newChild: Node, oldChild: Node): Node {
+  Node.prototype.replaceChild = function (this: Node, newChild: Node, oldChild: Node): Node {
     if (oldChild.parentNode !== this && oldChild.parentNode) {
       return originalReplaceChild.call(oldChild.parentNode, newChild, oldChild);
     }
